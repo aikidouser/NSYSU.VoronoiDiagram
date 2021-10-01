4efb2d6c94c0d87f996e5843a8945047de06a014
aikidouser <t36085520@icloud.com>
2021-10-01 18:57:01 +0800

    Finish the exe_btn GUI

--- setWindow.py
+++ setWindow.py
@@ -19,9 +19,14 @@ class MainWindow:
         # canvas part
         canvas_frame = tk.Frame(window, width=600, height=600, bg='orange')
 
+        # exe part
+        exe_btn_frame = tk.Frame(window, width=130, height=600)
+        sts_btn = tk.Button(exe_btn_frame, text='Step by Step', width=15, height=3)
+        run_btn = tk.Button(exe_btn_frame, text='Run', width=15, height=3)
+
         # place
         # place the read file part
-        read_file_frame.grid(column=0, row=0, padx=pad, pady=pad, sticky=tk.W + tk.N)
+        read_file_frame.grid(column=0, row=0, padx=pad, pady=pad, columnspan=2,sticky=tk.W + tk.N)
         file_path_msg.grid(column=0, row=0, padx=pad, pady=pad)
         file_input_btn.grid(column=1, row=0, padx=pad)
         enter_btn.grid(column=2, row=0, padx=pad)
@@ -29,6 +34,11 @@ class MainWindow:
         file_path_msg.insert('end', 'Hello World')
 
         # place the canvas part
-        canvas_frame.grid(column=0, row=1, padx=pad, pady=pad, sticky=tk.W + tk.N)
+        canvas_frame.grid(column=0, row=1, padx=pad, pady=pad, sticky=tk.N)
+
+        # place the exe button part
+        exe_btn_frame.grid(column=1, row=1, padx=pad, pady=pad, sticky=tk.W + tk.N)
+        sts_btn.grid(column=0, row=0, pady=2 * pad)
+        run_btn.grid(column=0, row=1, pady=2 * pad)
 
         window.mainloop()
