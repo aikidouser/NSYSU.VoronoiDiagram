1a6cdd98e694368bad3075095817fa575f1c2890
aikidouser <t36085520@icloud.com>
2021-12-15 11:31:36 +0800

    Rewrite the force way

--- VDAlgo.py
+++ VDAlgo.py
@@ -3,6 +3,7 @@ from os import write
 import re
 from shapely import geometry as geo
 
+
 def det(a, b):
     return a[0] * b[1] - a[1] * b[0]
 
@@ -12,9 +13,11 @@ class Bisector:
         self.p = (point_a, point_b)
         self.line = p_bisector
 
+
 class VoronoiDiagram:
     def __init__(self, point_list):
         self.point_list = sorted(point_list)
+        # self.point_list = point_list
         self.record = list()
         self.polyedge_list = list()     # for final result
         self.polypoints_list = list()   # for final result
@@ -24,43 +27,43 @@ class VoronoiDiagram:
     def run(self, type=0):
         print("Start")
 
-        if len(self.point_list) == 1:
-            temp_dict = {'points': self.point_list, 'edge': [[-1, -1], [-1, 1]]}
-            self.record.append(temp_dict)
+        # if len(self.point_list) == 1:
+        #     temp_dict = {'points': self.point_list,
+        #                  'edge': [[-1, -1], [-1, 1]]}
+        #     self.record.append(temp_dict)
 
-        elif len(self.point_list) == 2:
-            ppdc = self.__p_bisector(*self.point_list)
-            self.polyedge_list.append(ppdc)
-            self.polypoints_list.append(self.point_list)
-            temp_dict = {'points': self.point_list, 'edge': ppdc}
-            self.record.append(temp_dict)
+        # elif len(self.point_list) == 2:
+        #     ppdc = self.__p_bisector(*self.point_list)
+        #     self.polyedge_list.append(ppdc)
+        #     self.polypoints_list.append(self.point_list)
+        #     temp_dict = {'points': self.point_list, 'edge': ppdc}
+        #     self.record.append(temp_dict)
 
-        elif len(self.point_list) == 3:
-            self.__3p_force(self.point_list)
+        self.__force(self.point_list)
 
-    def __garbage(self, t_point_list):
+    def __garbage(self, point_list):
         # This can only be used in the case of three points.
-        print(t_point_list)
-        split = int(len(t_point_list)/2)
+        print(point_list)
+        split = int(len(point_list)/2)
         hyperplane_list = list()
 
-        if len(t_point_list) <= 1:
-            print(f"{t_point_list} return")
-            return t_point_list
+        if len(point_list) <= 1:
+            print(f"{point_list} return")
+            return point_list
 
-        elif len(t_point_list) == 2:
+        elif len(point_list) == 2:
             self.polyedge_list.append(
-                self.__p_bisector(t_point_list[0], t_point_list[1]))
-            self.polypoints_list.append([t_point_list[0], t_point_list[1]])
+                self.__p_bisector(point_list[0], point_list[1]))
+            self.polypoints_list.append([point_list[0], point_list[1]])
 
-            # t_point_list = sorted(t_point_list, key=lambda x: x[0]) 
+            # point_list = sorted(point_list, key=lambda x: x[0])
 
-            print(f"{t_point_list} return")
-            return t_point_list
+            print(f"{point_list} return")
+            return point_list
 
-        l_pointlist = self.__garbage(t_point_list[0: split])
+        l_pointlist = self.__garbage(point_list[0: split])
         print("finish left")
-        r_pointlist = self.__garbage(t_point_list[split:])
+        r_pointlist = self.__garbage(point_list[split:])
         print("finish right")
 
         # Merge
@@ -68,7 +71,8 @@ class VoronoiDiagram:
         l_point = l_pointlist[0]
         for r_point in r_pointlist:
             hyperplane = self.__p_bisector(l_point, r_point)
-            inters = self.__line_intersection(hyperplane, self.polyedge_list[0])
+            inters = self.__line_intersection(
+                hyperplane, self.polyedge_list[0])
 
             hyper_part1 = [hyperplane[0], inters]
             hyper_part2 = [inters, hyperplane[1]]
@@ -80,29 +84,40 @@ class VoronoiDiagram:
             elif(check2):
                 hyperplane = hyper_part2
 
-
             self.polyedge_list.append(hyperplane)
 
-    def __3p_force(self, point_list):
+    def __force(self, point_list):
         s_index = 0
         dis_list = list()
 
+        # TODO: check if need to merge
+        if len(point_list) == 1:
+            self.__writeback_record('n', point_list, None)
+            return
+
+        elif len(point_list) == 2:
+            p_bisector = self.__p_bisector(*self.point_list)
+            self.polyedge_list.append(p_bisector)
+            self.polypoints_list.append(point_list)
+            self.__writeback_record('n', point_list, p_bisector)
+            return
+
         for i in range(len(point_list)):
-            dis_list.append(math.dist(point_list[i], point_list[(i+1)%3]))
-        
+            dis_list.append(math.dist(point_list[i], point_list[(i+1) % 3]))
+
         s_index = (dis_list.index(max(dis_list)) + 1) % 3
         point_list += point_list[:s_index]
         del point_list[0:s_index]
-        
+
         def anti_clockwise(point_list):
             d = 0
-            for i in range(1,len(point_list)):
-                d += det(point_list[i-1],point_list[i])
-            d += (det(point_list[-1],point_list[0]))
+            for i in range(1, len(point_list)):
+                d += det(point_list[i-1], point_list[i])
+            d += (det(point_list[-1], point_list[0]))
             if d < 0:
                 point_list.reverse()
             return point_list
-        
+
         point_list = anti_clockwise(point_list)
         l_p_bisector = self.__p_bisector(point_list[0], point_list[1])
         r_p_bisector = self.__p_bisector(point_list[1], point_list[2])
@@ -123,11 +138,13 @@ class VoronoiDiagram:
             write_back()
             self.polyedge_list.append(m_p_bisector)
             self.polypoints_list.append([point_list[2], point_list[0]])
-            self.__writeback_record('n', point_list, l_p_bisector, r_p_bisector, m_p_bisector)
-        
+            self.__writeback_record(
+                'n', point_list, l_p_bisector, r_p_bisector, m_p_bisector)
+
         else:
             write_back()
-            self.__writeback_record('n', point_list, l_p_bisector, r_p_bisector)
+            self.__writeback_record(
+                'n', point_list, l_p_bisector, r_p_bisector)
 
     def __p_bisector(self, a, b):
         p_bisector = list()
@@ -137,8 +154,10 @@ class VoronoiDiagram:
         vector_extend = [i * 600 for i in normal_vector]
         print(f"{a} {b} {normal_vector} {vector_extend}")
 
-        start_p = [midpoint[0] - vector_extend[0], midpoint[1] - vector_extend[1]]
-        end_p = [midpoint[0] + vector_extend[0], midpoint[1] + vector_extend[1]]
+        start_p = [midpoint[0] - vector_extend[0],
+                   midpoint[1] - vector_extend[1]]
+        end_p = [midpoint[0] + vector_extend[0],
+                 midpoint[1] + vector_extend[1]]
         p_bisector = [start_p, end_p]
 
         print(f"bisector: {a}, {b} --> {p_bisector}")
